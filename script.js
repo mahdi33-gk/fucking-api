7e1388c8cefcf6900a2c354b8c201f8f469b6dbf
gk.khan <meheduvau@gmail.com>
2024-10-07 17:49:57 +0600

    api

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ console.log(user)
 function overboard(){
     fetch('https://openapi.programming-hero.com/api/peddy/pets')
        .then(response => response.json())
-       .then (json => console.log(json))
+       .then (data => dataeror(data))
 }
+function dataeror(data){
+    for(const user of data){
+        console.log(user.name)
+    }
+}
+
+function overboard(){
+    fetch('https://openapi.programming-hero.com/api/peddy/pets')
+       .then(response => response.json())
+       .then (data => console.log(data))
+}
+overboard();

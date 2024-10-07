5c930b3be4950c750c29cc066c6a1a316495eb11
gk.khan <meheduvau@gmail.com>
2024-10-07 17:05:09 +0600

    api

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const stringified = JSON.stringify(user)
 
 console.log(user)
 
+fetch('https://openapi.programming-hero.com/api/peddy/pets')
+.then(response => response.JSON())
+.then (JSON => console.log(JSON))
